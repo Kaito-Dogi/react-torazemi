486b9992cc05b5624547daf3b2ae28cf8611f297
Kaito Dogi <kendobu0405@gmail.com>
2022-07-04 04:01:21 +0900

    :sparkles: useState を使用

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,15 @@
+import {useState} from "react";
 import {Title, Content} from './index'
 
 const Article = (props) => {
+    const [isPublished, setIsPublished] = useState(false);
+    console.log(isPublished);
+
     return (
         <>
-            <Title title={props.title}/>
-            <Content content={props.content}/>
+            <Title title={props.title} />
+            <Content content={props.content} />
+            <button onClick={() => setIsPublished(true)}>公開</button>
         </>
     );
 };
