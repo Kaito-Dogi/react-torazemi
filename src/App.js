7f057dec3964ee3aeb12941e8d1b7c9c1eeb735b
Kaito Dogi <kendobu0405@gmail.com>
2022-07-04 10:13:32 +0900

    :sparkles: useEffect で github api を叩いた

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import {Article} from './components/index';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const baseUrl = 'https://api.github.com/users/';
+  const userId = 'Kaito-Dogi';
+
+  const [name, setName] = useState('');
+  const [avatarUrl, setAvatarUrl] = useState('');
+
+  const fetchUser = (userId) => {
+    fetch(`${baseUrl}${userId}`)
+      .then(res => res.json())
+      .then(data => {
+          console.log(data);
+          setName(data.name);
+          setAvatarUrl(data.avatar_url);
+        }
+      )
+      .catch(error => console.error(error));
+  }
+
+  useEffect(() => {
+    fetchUser(userId);
+  });
+
   return (
-    <Article
-      title={'師匠（@akt_prs10）のもとで React 入門します。'}
-      content={"がんばるます。"}
-    />
+    <>
+      <h1>{name}</h1>
+      <img src={avatarUrl} width={300} />
+      <Article
+        title={'師匠（@akt_prs10）のもとで React 入門します。'}
+        content={"がんばるます。"}
+      />
+    </>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
